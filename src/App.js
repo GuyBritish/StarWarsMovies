b7bb8b9fe46017f53d3643e3bba2d065d7777551
Max <80323354+GuyBritish@users.noreply.github.com>
2021-12-27 03:14:34 -0600

    Remove no movie message from error display

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
 			</section>
 			<section>
 				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-				{!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+				{!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
 				{isLoading && <p>Loading...</p>}
 				{!isLoading && error && <p>{error}</p>}
 			</section>
