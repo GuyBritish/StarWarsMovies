8de36e1e4b26dae171b1ac31fea3c7bbc1dcba1b
Max <80323354+GuyBritish@users.noreply.github.com>
2021-12-27 01:54:25 -0600

    Implement fetch movies

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+const axios = require("axios");
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	const fetchMoviesHandler = async () => {
+		const options = {
+			url: "https://swapi.dev/api/films/",
+		};
+		const resp = await axios(options);
+		const transformedMovies = resp.data.results.map((movie) => {
+			return {
+				id: movie.episode_id,
+				title: movie.title,
+				openingText: movie.opening_crawl,
+				releaseDate: movie.release_date,
+			};
+		});
+		setMovies(transformedMovies);
+	};
+
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
