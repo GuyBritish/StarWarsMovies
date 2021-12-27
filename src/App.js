6496b87dfdbdc57cbf6bdebf46016c56dc65dbab
Max <80323354+GuyBritish@users.noreply.github.com>
2021-12-27 06:44:21 -0600

    Implement fetching on start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -10,7 +10,7 @@ function App() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const fetchMoviesHandler = async () => {
+	const fetchMoviesHandler = useCallback(async () => {
 		setIsLoading(true);
 		setError(null);
 
@@ -33,7 +33,11 @@ function App() {
 			setError(err.message);
 		}
 		setIsLoading(false);
-	};
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
 
 	let content = <p>Found no movies.</p>;
 
